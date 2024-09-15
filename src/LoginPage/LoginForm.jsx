9267cc204d4ea05d95654c5c2ee7fa02cba88d05
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-09-15 11:50:02 +0530

    update login.jsx

--- src/LoginPage/LoginForm.jsx
+++ src/LoginPage/LoginForm.jsx
@@ -10,6 +10,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import backgroundImage from "../images/backnew.jpg";
 import Logo from "../images/BMKLogo.jpg"; // Adjusted path
 import Validation from "../LoginPage/Validation";
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 const defaultTheme = createTheme();
 
@@ -22,6 +26,7 @@ function LoginForm() {
   });
 
   const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
 
   function handleChange(e) {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -63,7 +68,7 @@ function LoginForm() {
                 navigate("/stocksupervisordashboard");
                 break;
               case "sitesupervisor":
-                navigate("/sitesupervisordashboard"); // Corrected this line to point to the right dashboard
+                navigate("/sitesupervisor"); // Corrected this line to point to the right dashboard
                 break;
               default:
                 throw new Error("Unknown role");
@@ -83,87 +88,111 @@ function LoginForm() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} elevation={6}>
-          <Box
+            <CssBaseline />
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: `url(${backgroundImage})`,
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} elevation={6}>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Box component="img" src={Logo} alt="Company Logo" sx={{ width: 100, height: 100, mb: 1 }} />
+                        <Typography component="h2" variant="h3">
+                            Tools Management System
+                        </Typography>
+                        <Typography component="h4" variant="h4">
+                            Login
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                id="outlined-username-input"
+                                label="Username"
+                                type="text"
+                                value={values.username}
+                                name='username'
+                                onChange={handleChange}
+                                autoComplete="current-username"
+                                fullWidth
+                                margin="normal"
+                                error={!!errors.username}
+                                helperText={errors.username}
+                            />
+                            <TextField
+                                id="outlined-password-input"
+                                label="Password"
+                                type={showPassword ? 'text' : 'password'} // Toggle password visibility
+                                value={values.password}
+                                name='password'
+                                onChange={handleChange}
+                                autoComplete="current-password"
+                                fullWidth
+                                margin="normal"
+                                error={!!errors.password}
+                                helperText={errors.password}
+                                InputProps={{
+                                    endAdornment: (
+                                        <IconButton
+                                            onClick={() => setShowPassword(!showPassword)}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    )
+                                }}
+                            />
+                            <Button
+                                type='submit'
+                                className='submit'
+                                variant="contained"
+                                color="primary"
+                                fullWidth
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Login
+                            </Button>
+
+        <Box
             sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100px', 
+                backgroundColor: '#f8f9fa',
+                textAlign: 'center', 
+                padding: '20px', 
+                borderTop: '1px solid #ddd', 
             }}
-          >
-            <Box
-              component="img"
-              src={Logo}
-              alt="Company Logo"
-              sx={{ width: 100, height: 100, mb: 1 }}
-            />
-            <Typography component="h2" variant="h3">
-              Tools Management System
+        >
+            <Typography variant="body2" color="textSecondary">
+                Copyright © 2024 Dilum BMK Engineers (Pvt) Ltd. All rights reserved.
             </Typography>
-            <Typography component="h4" variant="h4">
-              Login
+            <Typography variant="body2" color="textSecondary">
+                Concept & Design by 5<sup>th</sup>Batch - Group 02, Department of ICT_FOT_UOR
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                id="outlined-username-input"
-                label="Username"
-                type="text"
-                value={values.username}
-                name="username"
-                onChange={handleChange}
-                autoComplete="current-username"
-                fullWidth
-                margin="normal"
-                error={!!errors.username}
-                helperText={errors.username}
-              />
-              <TextField
-                id="outlined-password-input"
-                label="Password"
-                type="password"
-                value={values.password}
-                name="password"
-                onChange={handleChange}
-                autoComplete="current-password"
-                fullWidth
-                margin="normal"
-                error={!!errors.password}
-                helperText={errors.password}
-              />
-              <Button
-                type="submit"
-                className="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Login
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+        </Box>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
   );
 }
 
